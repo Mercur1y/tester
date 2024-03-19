f44234ab56d3ffaab7fe4f553b03aa5e99775356
Глеб Приставка <g.pristavka@bftcom.com>
2024-03-20 00:29:03 +0400

    Del DefaultRepository.java

--- src/main/java/com/sgtu/tester/common/mvc/DefaultRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sgtu.tester.common.mvc;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
-
-@NoRepositoryBean
-public interface DefaultRepository<E extends DefaultEntity> extends JpaRepository<E, Long> {
-}
